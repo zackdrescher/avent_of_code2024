949d3ffc95fd6054a8f7ae25d3939cb6a2c5e3e6
Zack Drescher <hello@zackdrescher.com>
2024-12-22 15:18:27 -0500

    wip part 2 + solution from reddit

--- aoc/day6/cli.py
+++ aoc/day6/cli.py
@@ -12,8 +12,45 @@ app = typer.Typer()
 def part1(file_path: Path) -> None:
     position, board = load_data(file_path)
 
-    board = traverse(position, board)
+    board, _ = traverse(position, board)
 
     result = sum(row.count("X") for row in board)
 
     typer.echo(result)
+
+
+@app.command()
+def part2(file_path: Path) -> None:
+    position, board = load_data(file_path)
+
+    board, result = traverse(position, board)
+
+    for position in result:
+        board[position[0]][position[1]] = "O"
+    typer.echo("\n".join("".join(row) for row in board))
+
+    typer.echo(len(result))
+
+
+@app.command()
+def cheat(file_path: Path) -> None:
+    G = {
+        i + j * 1j: c
+        for i, r in enumerate(open(file_path))
+        for j, c in enumerate(r.strip())
+    }
+
+    start = min(p for p in G if G[p] == "^")
+
+    def walk(G):
+        pos, dir, seen = start, -1, set()
+        while pos in G and (pos, dir) not in seen:
+            seen |= {(pos, dir)}
+            if G.get(pos + dir) == "#":
+                dir *= -1j
+            else:
+                pos += dir
+        return {p for p, _ in seen}, (pos, dir) in seen
+
+    path = walk(G)[0]
+    print(len(path), sum(walk(G | {o: "#"})[1] for o in path))
--- aoc/day6/traverse.py
+++ aoc/day6/traverse.py
@@ -11,7 +11,9 @@ def left_board(position: Position, height: int, width: int) -> bool:
     )
 
 
-def traverse(position: Position, board: Board) -> Board:
+def traverse(position: Position, board: Board) -> tuple[Board, set[Position]]:
+    start_position = position[:]
+
     height = len(board)
     width = len(board[0])
 
@@ -20,6 +22,9 @@ def traverse(position: Position, board: Board) -> Board:
         raise ValueError(msg)
 
     guard = "^"
+
+    loop_positions = set()
+    hits = set()
 
     while not left_board(position, height, width):
         board[position[0]][position[1]] = "X"
@@ -31,9 +36,63 @@ def traverse(position: Position, board: Board) -> Board:
             not left_board(next_postion, height, width)
             and board[next_postion[0]][next_postion[1]] == "#"
         ):
+            hits.add((position, guard))
             guard = GUARD_TRANSITIONS[guard]
             continue
 
+        if (
+            not left_board(next_postion, height, width)
+            and next_postion != start_position
+            and loop_check(
+                guard,
+                position,
+                board,
+                height,
+                width,
+                hits,
+            )
+        ):
+            loop_positions.add(next_postion)
+
         position = next_postion
 
-    return board
+    return board, loop_positions
+
+
+def loop_check(
+    guard: str,
+    position: Position,
+    board: Board,
+    height: int,
+    width: int,
+    hits: set[tuple[Position, str]],
+) -> bool:
+    board = [row.copy() for row in board]
+    move = GUARD_SYMBOLS[guard]
+    loop_hits: set[tuple[Position, str]] = set(hits)
+    board[position[0] + move[0]][position[1] + move[1]] = "#"
+    loop_hits.add((position, guard))
+
+    right_symbol = GUARD_TRANSITIONS[guard]
+    right_move = GUARD_SYMBOLS[right_symbol]
+
+    next_position = (position[0] + right_move[0], position[1] + right_move[1])
+
+    while not left_board(next_position, height, width):
+        next_occupant = board[next_position[0]][next_position[1]]
+
+        if next_occupant == "#":
+            if (position, right_symbol) in loop_hits:
+                return True
+
+            loop_hits.add((position, right_symbol))
+
+            right_symbol = GUARD_TRANSITIONS[right_symbol]
+            right_move = GUARD_SYMBOLS[right_symbol]
+
+        position = next_position
+        next_position = (
+            next_position[0] + right_move[0],
+            next_position[1] + right_move[1],
+        )
+    return False
